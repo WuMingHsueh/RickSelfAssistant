626341dc6ea079d5d8450c678e06759504bafc47
DAMASCUS\ri356 <ri3567@gmail.com>
2017-05-15 15:17:41 +0800

    initial dbconfig.php and will gitignore after all

---
+++ model/dbconfig.php
@@ -0,0 +1,10 @@
+<?php
+
+define('dbhost' , 'localhost');
+define('dbuser' , '');
+define('dbpassword' , '');
+define('dbname', '');
+define('dsn' , 'mysql:host=' . dbhost . ';dbname='.dbname.';charset=utf8');;
+
+
+?>
