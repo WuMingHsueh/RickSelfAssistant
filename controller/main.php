86e6a19569eeb56bbafc89695c951f92f88c9ea9
Wu Ming-Hsueh <ri3567@gmail.com>
2017-09-08 15:05:55 +0800

    完成DbProvider 寫入功能

--- controller/main.php
+++ controller/main.php
@@ -6,8 +6,25 @@ use \WuMingHsueh\SelfAssistant\Model\DbWork\DbProvider;
 // $userModel = new DbProvider('pgsql:host=127.0.0.1;dbname=teashop' , 'ri3567' , 'IISuess1120');
 // echo $userModel -> test();
 
-$dbProviderTest = new DbProvider();
-echo $dbProviderTest->test();
+$dsn = 'sqlsrv:Server=10.10.51.107,1433;Database=NORTHWND';
+$dsn = 'dblib:host=10.10.51.107:1433;dbname=NORTHWND';
+$sql = "select * from Territories";
+
+try {
+    $dbProviderTest = new \PDO($dsn , 'ri3567' , 'IISuess1120');
+    $dbProviderTest ->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);    
+    $pdoState = $dbProviderTest -> prepare($sql);
+    $pdoState -> execute();
+    $data = $pdoState -> fetchAll(\PDO::FETCH_ASSOC);
+    print_r($data);
+
+    
+} catch (\PDOException $e) {
+    echo $e->getMessage();
+}
+// echo $dbProviderTest->query($sql , []);
+
+
 
 
 
--- model/DbProvider.php
+++ model/DbProvider.php
@@ -14,9 +14,9 @@ class DbProvider
     private $pdoState;
     private $sql;
 
-    private $insertTemplete = "insert into :table: ( :field: ) values ";
-    private $updateTemplete = "update :table: set :fieldSet: ";
-    private $deleteTemplete = "delete from :table: ";
+    private $insertTemplete = 'insert into ":table:" ( :field: ) values ';
+    private $updateTemplete = 'update ":table:" set :fieldSet: ';
+    private $deleteTemplete = 'delete from ":table:" ';
 
     public function __construct(){
         $this -> pdo = new \PDO(DbConfig::DSN , DbConfig::DB_USER , DbConfig::DB_PASSWORD)
@@ -24,7 +24,6 @@ class DbProvider
 
         $a = \func_get_args();
         $i = \func_num_args();
-
         if(method_exists($this , $f = '__construct'.$i) && ($i != 0))
             \call_user_func_array([$this , $f] , $a);
     }
@@ -34,25 +33,69 @@ class DbProvider
             or die("Unable to connect to  Database");
     }
 
-    public function test(Type $var = null)
+    public function test()
     {
-        $sql = "select * from film";
-        $this-> pdoState = $this -> pdo -> prepare($sql);
-        $this-> pdoState -> execute();
-        $data = $this-> pdoState -> fetchAll(\PDO::FETCH_ASSOC);
+        // $sql = "select * from tea";
+        // $this-> pdoState = $this -> pdo -> prepare($sql);
+        // $this-> pdoState -> execute();
+        // $data = $this-> pdoState -> fetchAll(\PDO::FETCH_ASSOC);
+        // return json_encode($data);
         
-        return json_encode($data);
+        $parameter[] = ['Name' => 'kobe' , 'Price' => 400 , 'NowTime' => date('Y-m-d H:i:s')];
+        $parameter[] = ['Name' => 'iverson' , 'Price' => 90000 , 'NowTime' => date('Y-m-d H:i:s')];
+        $parameter[] = ['Name' => 'rick' , 'Price' => 3250 , 'NowTime' => date('Y-m-d H:i:s')];
+        $this -> insert( "NameInsert" , ['Name' , 'Price' , 'NowTime'] , $parameter);
+        
     }
 
-    private function formatSqlString ($table , $field , $sqlTemplete)
+    private function prepareSqlTable ($table , $sqlTemplete)
     {
-        // :table: relace   
-        $querySql = str_ireplace(":table:" , $table , $sqlTemplete);
+        // :table: relace  and initial sql 
+        $this -> sql = str_ireplace(":table:" , $table , $sqlTemplete);
     }
 
-    public function insert($table ,$field, $parmeter)
+    private function prepareSqlWhere( $fields , $parameter)
     {
-        # code...
+        $tempArray = [];
+        foreach ($fields as $key => $field) 
+            $tempArray[] = $field . " = " . $parameter[$key];
+        $this -> sql = $this -> sql ." where ". \implode(' and ' , $tempArray);
+    }
+
+    private function prepareSqlInsert( $table , $fields , $parameter)
+    {
+        $this -> prepareSqlTable($table , $this->insertTemplete);
+        
+        $tempSql = str_ireplace(":field:",  '"'. \implode('" , "' , $fields) .'"' ,$this -> sql );
+
+        if (count( array_filter( $parameter , "is_array" ) ) != 0)
+        {
+            $insertSqlValue = [];
+            foreach ($parameter as $rowData) 
+                $insertSqlValue[] = '( '.implode(' , ',array_fill(0 , count($rowData) , '?')) . ' )';
+            $tempSql .= \implode(' , ' , $insertSqlValue);
+        } else {
+            $tempSql .= '( '.implode(' , ',array_fill(0 , count($parameter) , '?')) . ' )';
+        }
+
+        $this-> sql = $tempSql;
+    }
+
+    public function insert($table ,$field, $parameter)
+    {
+        $this->prepareSqlInsert($table ,$field, $parameter);
+
+        $insertparameter = [];
+        if (count( array_filter( $parameter , "is_array" ) ) != 0) {
+            foreach ($parameter as $rowData) 
+                $insertparameter =  array_merge( $insertparameter , array_values($rowData) );
+        } else 
+            $insertparameter = array_values($parameter);
+        
+        $this -> pdo -> beginTransaction();
+        $status = $this -> tranctions($this->sql , $insertparameter);
+        $this -> pdo -> commit();
+        return $status;
     }
 
     public function update(Type $var = null)
@@ -68,6 +111,16 @@ class DbProvider
     public function select(Type $var = null)
     {
         # code...
+    }
+
+    public function tranctions($sql , $parameter)
+    {
+        try {
+            $this -> pdoState = $this -> pdo -> prepare($sql);
+            return ( $this -> pdoState -> execute($parameter) ) ? true : false;
+        } catch (\PDOException $e) {
+            die("Error!: " . $e -> getMessage() );
+        }
     }
     
     public function query($sql , $parameter)
